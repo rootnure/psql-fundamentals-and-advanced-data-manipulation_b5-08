8221651ac7610c315b46550523d5e8ce94b3351b
rootnure <nur.diu.2791@gmail.com>
2025-06-30 11:29:59 +0600

    Data Filtering, Arithmetic Operators

--- select.sql
+++ select.sql
@@ -11,17 +11,56 @@ CREATE Table students (
     country VARCHAR(30)
 );
 
--- Show all column data --> *
+-- SELECT all column data --> *
 SELECT * FROM students;
 
--- Show specific column data
+-- SELECT specific column data
 SELECT email FROM students;
 
--- Show specific column data with name alias
+-- SELECT specific column data with name alias
 SELECT email as "Student Email", age as "Student Age" FROM students;
 
--- Show data in sorting order
+-- SELECT data in sorting order
 -- ascending order on "age"
 SELECT * FROM students ORDER BY age ASC;
 -- descending order on "age"
 SELECT * FROM students ORDER BY age DESC;
+
+-- SELECT unique data form a column
+SELECT DISTINCT country FROM students;
+
+SELECT DISTINCT blood_group FROM students;
+
+-- SELECT conditionally using WHERE
+SELECT * FROM students WHERE country = 'Australia';
+
+-- SELECT conditionally using WHERE (multiple true conditions)
+SELECT * FROM students WHERE grade = 'A' AND course = 'Physics';
+
+-- SELECT conditionally using WHERE (multiple conditions, minimum 1 true)
+SELECT *
+FROM students
+WHERE
+    country = 'USA'
+    OR country = 'Australia';
+
+-- SELECT conditionally using WHERE (multiple conditions, mix/matched)
+SELECT *
+FROM students
+WHERE (
+        country = 'USA'
+        OR country = 'Australia'
+    )
+    AND age = 18;
+
+-- SELECT conditionally using WHERE (arithmetic operators)
+-- Grater Than (>)
+SELECT * FROM students WHERE age > 18;
+-- Grater Than or Equal (>=)
+SELECT * FROM students WHERE age >= 18;
+-- Less Than (<)
+SELECT * FROM students WHERE age < 18;
+-- Less Than or Equal (<=)
+SELECT * FROM students WHERE age <= 18;
+-- Not Equal (<>) [Less Than or Grater than]
+SELECT * FROM students WHERE age <> 18;
